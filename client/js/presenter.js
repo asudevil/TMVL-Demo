903c8eb9469c3ac4f7fb3a6d3ac0cd75fd1ef3e6
admin <admin@admins-MacBook-Pro.local>
2015-12-25 17:53:07 -0800

    first commit

--- client/js/presenter.js
+++ client/js/presenter.js
@@ -20,6 +20,10 @@ var Presenter = {
         var self = this;
         var element = event.target;
         var videoUrl = element.getAttribute("vidUrl")
+        
+        templateURL = element.getAttribute("template"),
+        presentation = element.getAttribute("presentation");
+        
         if (videoUrl) {
             var player = new Player();
             var playlist = new Playlist();
@@ -29,5 +33,56 @@ var Presenter = {
             player.playlist.push(mediaItem);
             player.present();
         }
-    }
+        
+        if(templateURL) {
+        self.showLoadingIndicator(presentation);
+        resourceLoader.loadResource(templateURL,
+            function(resource) {
+                if (resource) {
+                    var doc = self.makeDocument(resource);
+                    doc.addEventListener("select", self.load.bind(self));
+                    if (self[presentation] instanceof Function) {
+                        self.defaultPresenter.call(self, doc);
+                        }    
+                    }
+                }
+            );
+        }
+    },
+    
+    showLoadingIndicator: function(presentation) {
+        if (!this.loadingIndicator) {
+            this.loadingIndicator = this.makeDocument(this.loadingTemplate);
+        }
+        if (!this.loadingIndicatorVisible && presentation != "modalDialogPresenter" && presentation != "menuBarItemPresenter") {
+            navigationDocument.pushDocument(this.loadingIndicator);
+            this.loadingIndicatorVisible = true;
+        }
+    },
+
+    removeLoadingIndicator: function() {
+        if (this.loadingIndicatorVisible) {
+            navigationDocument.removeDocument(this.loadingIndicator);
+            this.loadingIndicatorVisible = false;
+        }
+    },
+
+
+    defaultPresenter: function(xml) {
+        if(this.loadingIndicatorVisible) {
+            navigationDocument.replaceDocument(xml, this.loadingIndicator);
+            this.loadingIndicatorVisible = false;
+        } else {
+            navigationDocument.pushDocument(xml);
+        }
+    },
+
+      loadingTemplate: `<?xml version="1.0" encoding="UTF-8" ?>
+        <document>
+          <loadingTemplate>
+            <activityIndicator>
+              <text>Loading...</text>
+            </activityIndicator>
+          </loadingTemplate>
+        </document>`
 }
--- client/templates/devslopestv.xml.js
+++ client/templates/devslopestv.xml.js
@@ -16,38 +16,53 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
     <list>
       <section>
         <header>
-          <title>Spicy Suya!!</title>
+          <title style="background-color: rgba(240, 240, 240, 0.7); tv-text-style: title; font-size:48; font-weight:regular;">Featured Movies!!</title>
         </header>
         <listItemLockup vidUrl="https://s3.amazonaws.com/spicysuya/Oakland_Video.mp4">
-          <title>Fun in Oakland!!</title>
+          <title style="background-color: rgba(240, 240, 240, 0.5)"> Fun in Oakland!!</title>
           <relatedContent>
             <lockup>
               <img src="${this.BASEURL}resources/OaklandFestival.jpg" width="857" height="482" />
               <title>Fun in Oakland!</title>
-              <subtitle>Oakland African Festival</subtitle>
-              <description>A great event in Oakland featuring great music, dancing, food, vendors, and other fun stuff!!</description>
+              <subtitle style="background-color: rgb(254, 150, 0)">Oakland African Festival</subtitle>
+              <description style="background-color: rgb(254, 150, 0)">A great event in Oakland featuring great music, dancing, food, vendors, and other fun stuff!!</description>
             </lockup>
           </relatedContent>
         </listItemLockup>
-        <listItemLockup>
-          <title>Bouqui!!</title>
+
+
+        <listItemLockup template="${this.BASEURL}/templates/Compilation.xml.js" presentation="modalDialogPresenter">
+          <title style="background-color: rgba(240, 240, 240, 0.5)">Bouqui!!</title>
           <relatedContent>
             <lockup>
               <img src="${this.BASEURL}resources/bouqui.jpg" width="857" height="482" />
               <title>Bouqui Promotional movie</title>
-              <subtitle>Bouqui's movie</subtitle>
-              <description>Bouqui is a fantastic artist.  This is a tv serious about her experience in the united states as an African artist trying to make it in the united states.  Alot of African Diaspora can relate to this movie. </description>
+              <subtitle style="background-color: rgb(254, 150, 0)">Bouqui's movie</subtitle>
+              <description style="background-color: rgb(254, 150, 0)">Bouqui is a fantastic artist.  This is a tv serious about her experience in the united states as an African artist trying to make it in the united states.  Most of the African Diaspora population can relate to this movie. </description>
             </lockup>
           </relatedContent>
         </listItemLockup>
+
+
         <listItemLockup>
-          <title>Title 3</title>
+          <title style="background-color: rgba(240, 240, 240, 0.5)">Title 3</title>
           <relatedContent>
             <lockup>
               <img src="${this.BASEURL}resources/spicysuyabanner.png" width="857" height="482" />
               <title>Related Title 3</title>
-              <subtitle>Related Subtitle 3</subtitle>
-              <description>Lorem ipsum Sociis mauris in integer, a dolor netus non dui aliquet, sagittis felis sodales, dolor sociis mauris, vel eu libero cras. Interdum at. Eget habitasse elementum est, ipsum purus pede porttitor class, ut adipiscing, aliquet sed auctor, imperdiet arcu per diam dapibus libero duis. Enim eros in vel, volutpat.</description>
+              <subtitle style="background-color: rgb(254, 150, 0)">Related Subtitle 3</subtitle>
+              <description style="background-color: rgb(254, 150, 0)">Lorem ipsum Sociis mauris in integer, a dolor netus non dui aliquet, sagittis felis sodales, dolor sociis mauris, vel eu libero cras. Interdum at. Eget habitasse elementum est, ipsum purus pede porttitor class, ut adipiscing, aliquet sed auctor, imperdiet arcu per diam dapibus libero duis. Enim eros in vel, volutpat.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        <listItemLockup>
+          <title style="background-color: rgba(240, 240, 240, 0.5)">Title 4</title>
+          <relatedContent>
+            <lockup>
+              <img src="${this.BASEURL}resources/spicysuyabanner.png" width="857" height="482" />
+              <title>Related Title 4</title>
+              <subtitle style="background-color: rgb(254, 150, 0)">Related Subtitle 3</subtitle>
+              <description style="background-color: rgb(254, 150, 0)">Lorem ipsum Sociis mauris in integer, a dolor netus non dui aliquet, sagittis felis sodales, dolor sociis mauris, vel eu libero cras. Interdum at. Eget habitasse elementum est, ipsum purus pede porttitor class, ut adipiscing, aliquet sed auctor, imperdiet arcu per diam dapibus libero duis. Enim eros in vel, volutpat.</description>
             </lockup>
           </relatedContent>
         </listItemLockup>
